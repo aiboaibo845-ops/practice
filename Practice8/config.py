f043d38cf6d012162267c0f837da93eb4f414ae3
aiboaibo845-ops <aiboaibo845@gmail.com>
2026-04-21 11:13:16 +0500

    Add files via upload

---
+++ Practice8/config.py
@@ -0,0 +1,9 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "phonebook_db", # имя моей бд
+        "user": "postgres",         # мой пользователь бд
+        "password": "1234" ,# мой пароль
+        "client_encoding": "utf8"   
+    }
+    
